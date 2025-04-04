16eba0bc629f4a6b09f2113d02f967d01ad97572
wed-4 <sedwey234@gmail.com>
2025-04-04 23:37:59 +0900

    add vga

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,23 @@ use core::panic::PanicInfo;
 
 use hlt::hlt_loop;
 
-
 mod hlt;
-
+mod vga_buffer;
 
 /// This function is called on panic.
 #[panic_handler]
 pub fn panic(_info: &PanicInfo) -> ! {
     // This function is called on panic
     // You can log the panic information or take other actions here
-    hlt_loop();
+    println!("Hello World{}", "!");
+
+    loop {}
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     // This is where the program starts executing
     // Initialize the system, peripherals, etc.
-    
 
-    hlt_loop();
+    loop {}
 }
